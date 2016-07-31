b31d5e2223f02fe12be6299b71e5c2a3d83d26ee
Florian <dekontaminator@googlemail.com>
2016-07-31 12:59:10 +0200

    initial comic con price worker

--- src/main/java/de/florianmarsch/ConfigJob.java
+++ src/main/java/de/florianmarsch/ConfigJob.java
@@ -1,48 +1,50 @@
 package de.florianmarsch;
 
-import java.util.List;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.SchedulerException;
-
-import de.florianmarsch.football_api.Accessor;
-import de.florianmarsch.football_api.vo.Ligue;
-import de.florianmarsch.football_api.vo.Week;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConfigJob implements Job {
 
-	private Ligue ligue;
 
-	
+	final static Logger logger = LoggerFactory.getLogger(ConfigJob.class);
 
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		if (ligue == null) {
-			try {
-				ligue = (Ligue) context.getScheduler().getContext().get("ligue");
-				if (ligue == null) {
-					throw new RuntimeException("Ligue not configed");
-				}
-			} catch (SchedulerException e) {
-				throw new RuntimeException(e.getMessage());
-			}
-		}
-		
-		Accessor accessor = new Accessor();
-		List<Week> weeks = accessor.getWeeks(ligue);
-		Week current = null;
-		for (Week week : weeks) {
-			if(week.isActive()){
-				current = week;
-			}
-		}
-		try {
-			context.getScheduler().getContext().put("week", current);
-		} catch (SchedulerException e) {
-			e.printStackTrace();
-		}
+		String content = loadFile("http://comic-con-price-api.herokuapp.com/api/comiccon/2017/");
+		logger.info(content);
 	}
 
+	
+	private String loadFile(String url) {
+
+		StringBuffer tempReturn = new StringBuffer();
+		try {
+			URL u = new URL(url);
+			InputStream is = u.openStream();
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
+			String s;
+
+			while ((s = dis.readLine()) != null) {
+				tempReturn.append(s);
+			}
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tempReturn.toString();
+	}
 
 }
